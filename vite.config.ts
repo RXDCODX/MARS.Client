5718240cd35561bdeddaa0009d9eb0f5a494fd4f
RXDCODX <maxxxes73rus@gmail.com>
2025-08-14 21:05:17 +0400

    Add viteSingleFile plugin to Vite configuration for single-file output

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   assetsInclude: ["**/*.webm", "**/*.mp4"],
   resolve: {
     alias: {
