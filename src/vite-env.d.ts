e513a3693f903f6a7b433fbcd8935b0824ae97f5
RXDCODX <maxxxes73rus@gmail.com>
2025-08-27 23:57:39 +0400

    Update TypeScript configuration and enhance component modules
    - Added support for SCSS module declarations in vite-env.d.ts to improve styling capabilities.
    - Updated tsconfig.app.tsbuildinfo to include new BackupPage components and related files for better project structure.
    - Updated install-state.gz to reflect changes in dependencies.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -18,3 +18,8 @@ declare module "*.mp4" {
   const src: string;
   export default src;
 }
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
