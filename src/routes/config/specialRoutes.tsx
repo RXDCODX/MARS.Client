6f4a329e1e73a97c269d795f39921a9d8cd98bd9
RXDCODX <maxxxes73rus@gmail.com>
2025-08-31 10:54:22 +0400

    Remove LinkTreeMenu component and associated styles
    - Deleted LinkTreeMenu.tsx and LinkTreeMenu.css files to eliminate unused components and streamline the codebase.
    - Updated specialRoutes.tsx to remove the LinkTreeMenu route configuration.

--- src/routes/config/specialRoutes.tsx
+++ src/routes/config/specialRoutes.tsx
@@ -1,6 +1,5 @@
 import ScoreboardAdminPanel from "@/components/OBS_Components/Scoreboard/AdminPanel/AdminPanel";
 
-import { LinktreeMenu } from "../LinkTree/LinkTreeMenu";
 import { RouteConfig } from "./RouteConfig";
 
 // Массив специальных маршрутов
@@ -10,11 +9,5 @@ export const specialRoutes: RouteConfig[] = [
     name: "Админ панель скорборда",
     type: "special",
     element: <ScoreboardAdminPanel />,
-  },
-  {
-    path: "/linktree",
-    name: "Linktree меню",
-    type: "special",
-    element: <LinktreeMenu />,
   },
 ];
