9fa0dcccb6a4577c731194b6d53aa2d7bde24155
RXDCODX <maxxxes73rus@gmail.com>
2026-04-20 00:56:19 +0400

    refactor: изменить цвет фона на прозрачный в нескольких компонентах и оптимизировать стили

--- src/components/OBS_Components/SoundRequest/Player/Background/LiquidChrome.tsx
+++ src/components/OBS_Components/SoundRequest/Player/Background/LiquidChrome.tsx
@@ -20,7 +20,7 @@ export const LiquidChrome = () => {
     const container = containerRef.current;
     const renderer = new Renderer({ antialias: true });
     const gl = renderer.gl;
-    gl.clearColor(1, 1, 1, 1);
+    gl.clearColor(0, 0, 0, 0);
 
     const vertexShader = `
       attribute vec2 position;
